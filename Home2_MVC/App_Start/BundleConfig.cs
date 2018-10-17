5986ddfb30ab36e55ef69002a94308afaaec8adf
Hoch <kolya.goch@gmail.com>
2018-10-17 11:59:37 +0300

    some changes 17.10

--- Home2_MVC/App_Start/BundleConfig.cs
+++ Home2_MVC/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Home2_MVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/save").Include(
+               "~/Scripts/save.js"));
         }
     }
 }
---
+++ Home2_MVC/Scripts/save.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $('#close').onclick = function () {
+        let name = $('#Name').val();
+        let phoneNumber = $('#PhoneNumber').val();
+        console.log("33333");
+    };
+});
